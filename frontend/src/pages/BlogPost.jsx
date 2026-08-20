465ed678937f9dfa6575d709896b52331e204f6b
Manish Jangid <manishjangidmakrana@gmail.com>
2026-08-20 11:50:40 +0530

    some changes

--- frontend/src/pages/BlogPost.jsx
+++ frontend/src/pages/BlogPost.jsx
@@ -207,7 +207,7 @@ export default function BlogPost() {
           </div>
         </header>
 
-        <div className="max-w-5xl container-x">
+        <div className="max-w-5xl container-x mb-[64px] md:mb-[96px]">
           <div className="md:p-6 p-4 rounded-md border border-burgundy-100 bg-white  position-relative -m-[90px]">
             {/* ══════════════════════════════════════════════
             2. COVER IMAGE
